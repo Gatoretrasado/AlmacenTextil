a51ff33c94ee024b21d66686dbdfd77cf047cf0f
David Algás <david_algas@hotmail.com>
2015-05-22 01:08:30 +0200

    Factura Inserta Bien falta lo del Fichero

--- src/almacentextil/FacturaC.java
+++ src/almacentextil/FacturaC.java
@@ -2,21 +2,23 @@ package almacentextil;
 
 import java.awt.*;
 import java.awt.event.*;
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.sql.*;
 import javax.swing.*;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 
 public final class FacturaC extends JFrame {
 
+    private ArrayList<Integer> IDs = new ArrayList<>();
     private final JTabbedPane pestañas;
     private JPanel pestaña01, pestaña02, pestaña03;
-    static String nFactura;
     private String[] AÑOS, DIAS;
-    private JTextField txt_IDFactura;
-    private int ultimoID;
+    private JTextField txt_IDFactura, txt_TotalnoIVA;
+    private int ultimoID, precioNoIVA = 0;
     final String[] MESES = {"Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+    private JComboBox cmb_IDPedido;
 
     //Para poder conectarse a la base de datos
     private final conexionDB meConecto = new conexionDB();
@@ -46,9 +48,10 @@ public final class FacturaC extends JFrame {
         pestañas.addTab("Modificar", pestaña02);
         pestañas.addTab("Mostrar", pestaña03);
         topPanel.add(pestañas, BorderLayout.CENTER);
-        
+
+        ejecutarLineaPedido_Cli();
         ejecutarFactura_Cli();
-                
+
         setVisible(true);
     }
 
@@ -70,42 +73,51 @@ public final class FacturaC extends JFrame {
         lbl_IDPedido.setBounds(10, 60, 80, 20);
         pestaña01.add(lbl_IDPedido);
 
-        JComboBox cmb_IDPedido = new JComboBox();
+        cmb_IDPedido = new JComboBox();
         cmb_IDPedido.setBounds(100, 60, 150, 20);
+        cmb_IDPedido.addActionListener(new ActionListener() { // Accion a realizar cuando el JComboBox cambia de item seleccionado.
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Busqueda();
+            }
+        });
         pestaña01.add(cmb_IDPedido);
 
-        JLabel lbl_Fechahoy = new JLabel("Fecha Factura:");
+        JLabel lbl_Fechahoy = new JLabel("Fecha Hoy:");
         lbl_Fechahoy.setBounds(10, 105, 80, 20);
         pestaña01.add(lbl_Fechahoy);
 
-        JComboBox cmboxDias = new JComboBox(DIAS);
-        cmboxDias.setBounds(100, 105, 40, 20);
-        cmboxDias.setSelectedIndex(1);
-        pestaña01.add(cmboxDias);
-
-        JComboBox cmboxMeses = new JComboBox(MESES);
-        cmboxMeses.setBounds(150, 105, 85, 20);
-        pestaña01.add(cmboxMeses);
-
-        JComboBox cmboxYear = new JComboBox(AÑOS);
-        cmboxYear.setBounds(250, 105, 80, 20);
-        pestaña01.add(cmboxYear);
+        JTextField txt_fechaHoy = new JTextField();
+        txt_fechaHoy.setBounds(100, 105, 80, 20);
+        txt_fechaHoy.setEditable(false);
+        txt_fechaHoy.setText(fechaActual());
+        pestaña01.add(txt_fechaHoy);
 
         JLabel lbl_TotalnoIVA = new JLabel("Total sin IVA:");
         lbl_TotalnoIVA.setBounds(10, 155, 80, 20);
         pestaña01.add(lbl_TotalnoIVA);
 
-        JTextField txt_TotalnoIVA = new JTextField();
+        txt_TotalnoIVA = new JTextField();
         txt_TotalnoIVA.setBounds(100, 155, 150, 20);
         txt_TotalnoIVA.setEditable(false);
         pestaña01.add(txt_TotalnoIVA);
 
-        JLabel lbl_Descuento = new JLabel("Descuento:");
-        lbl_Descuento.setBounds(10, 200, 80, 20);
+        JLabel lbl_Descuento = new JLabel("Descuento x%:");
+        lbl_Descuento.setBounds(10, 200, 100, 20);
         pestaña01.add(lbl_Descuento);
 
         JTextField txt_Descuento = new JTextField();
-        txt_Descuento.setBounds(100, 200, 150, 20);
+        txt_Descuento.setBounds(110, 200, 150, 20);
+        txt_Descuento.addKeyListener(new KeyAdapter() {    //Solo caracteres numericos y solo 9 digitos
+            @Override
+            public void keyTyped(KeyEvent e) {
+                int limitador = 3;
+                char caracter = e.getKeyChar();
+                if (((caracter < '0') || (caracter > '9')) && (caracter != '\b') || txt_Descuento.getText().length() >= limitador) {
+                    e.consume();
+                }
+            }
+        });
         pestaña01.add(txt_Descuento);
 
         JLabel lbl_TotalIVA = new JLabel("Total con IVA:");
@@ -120,18 +132,97 @@ public final class FacturaC extends JFrame {
         JButton btn_Aceptar = new JButton("Aceptar");
         btn_Aceptar.setBounds(200, 300, 90, 27);
         pestaña01.add(btn_Aceptar);
-        
+
         JButton btn_Limpiar = new JButton("Limpiar");
         btn_Limpiar.setBounds(300, 300, 90, 27);
         pestaña01.add(btn_Limpiar);
+
+        JButton btn_Calc = new JButton("Calcular");
+        btn_Calc.setBounds(300, 255, 90, 27);
+        pestaña01.add(btn_Calc);
+
+        btn_Limpiar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                try {
+                    txt_Descuento.setText("");
+                    cmb_IDPedido.setSelectedItem(0);
+
+                } catch (Exception err) {
+                    System.out.println("Error: " + err);
+                }
+            }
+        });
+
+        btn_Calc.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent evt) {
+                float precioIVA = 0;
+                float precioSinIVA = Float.parseFloat(txt_TotalnoIVA.getText());
+                float descuento;
+
+                if (txt_Descuento.getText().equalsIgnoreCase("")) {
+
+                    txt_Descuento.setText("0");
+                    descuento = 0;
+                    precioIVA = (int) ((precioSinIVA - (precioSinIVA * (descuento / 100))) * 1.21);
+                    txt_TotalIVA.setText(Integer.toString((int) precioIVA));
+
+                } else {
+                    descuento = Float.parseFloat(txt_Descuento.getText());
+                    precioIVA = (int) ((precioSinIVA - (precioSinIVA * (descuento / 100))) * 1.21);
+                    txt_TotalIVA.setText(Integer.toString((int) precioIVA));
+                }
+            }
+        });
 
         btn_Aceptar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent evt) {
                 try {
 
-                    nFactura = txt_IDFactura.getText();
-                    //imprimir(txt_idPed.getText(), .getText(), txt_totSin.getText(), txt_desc.getText(), tot_con1.getText());
+                    boolean insertado = false;
+                    String NULL = null;
+                    String nFactura = txt_IDFactura.getText();
+                    String idPedido = Integer.toString((int) cmb_IDPedido.getSelectedItem());
+                    String fecha = fechaActual();
+                    String totalSin = txt_TotalnoIVA.getText();
+                    String descuento = txt_Descuento.getText();
+                    if (descuento.equalsIgnoreCase("")) {
+                        descuento = "0";
+                    }
+                    String totalCon = txt_TotalIVA.getText();
+
+                    if (totalCon.equalsIgnoreCase("")) {
+                        JOptionPane.showMessageDialog(null, "Total Vacio", "Aviso", JOptionPane.INFORMATION_MESSAGE);
+                    } else {
+                        Connection miConexion = (Connection) meConecto.ConectarMysql();
+
+                        try (Statement st = miConexion.createStatement()) {
+
+                            //Para ejecutar la consulta
+                            String query = "INSERT INTO `almacentextil`.`factura_cli` (`Id_factura`, `Id_pedido`, `Fecha_factura`, `Total_sin_IVA`, `DTO`, `Total_con_IVA`) VALUES(" + NULL + ", '" + idPedido + "', '" + fecha + "', '" + totalSin + "', '" + descuento + "', '" + totalCon + "')";
+
+                            Statement s = miConexion.createStatement();
+                            st.executeUpdate(query);
+                            insertado = true;
+
+                            miConexion.close();
+
+                            if (insertado == true) {
+                                JOptionPane.showMessageDialog(null, "Insertado Con Exito!", "Guardado", JOptionPane.INFORMATION_MESSAGE);
+                                ejecutarLineaPedido_Cli();
+                                ejecutarFactura_Cli();
+                            }
+
+                        } catch (Exception ex) {
+                            JOptionPane.showMessageDialog(null, "Se ha producido un Error", "Error", JOptionPane.ERROR_MESSAGE);
+                            ex.printStackTrace();
+                            insertado = false;
+                        }
+
+                        //imprimir(nFactura, idPedido, fecha, totalSin, descuento, totalCon);
+                    }
 
                 } catch (Exception err) {
                     System.out.println(err);
@@ -302,23 +393,29 @@ public final class FacturaC extends JFrame {
         });
     }
 
-    static public void imprimir(String id_p, String fecha, String totalsinIVA, String destuento, String totalconIVA) throws IOException {
+    static public void imprimir(String nFactura, String id_produc, String fecha, String totalsinIVA, String descuento, String totalconIVA) throws IOException {
 
         BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\joseantonio\\Documents\\NetBeansProjects\\AlmacenTextil\\src\\Facturas\\Factura" + nFactura + ".txt"));
 
         bw.write("Numero de factura: " + nFactura);
         bw.newLine();
-        bw.write("Numero del pedido: " + id_p);
+        bw.write("Numero del pedido: " + id_produc);
         bw.newLine();
         bw.write("Fecha de la factura: " + fecha);
         bw.newLine();
         bw.write("Total (sin IVA): " + totalsinIVA + "€");
         bw.newLine();
-        bw.write("Descuento aplicado: " + destuento + "%");
+        bw.write("Descuento aplicado: " + descuento + "%");
         bw.newLine();
         bw.write("Total (con IVA): " + totalconIVA + "€");
         bw.newLine();
         bw.close();
+    }
+
+    public static String fechaActual() {
+        Date ahora = new Date();
+        SimpleDateFormat formateador = new SimpleDateFormat("yyyy-MM-dd");
+        return formateador.format(ahora);
     }
 
     void cargarFechas() {
@@ -364,5 +461,73 @@ public final class FacturaC extends JFrame {
 
         ultimoID = ultimoID + 1;
         txt_IDFactura.setText(Integer.toString(ultimoID));
+    }
+
+    public void ejecutarLineaPedido_Cli() {
+
+        cmb_IDPedido.removeAllItems();
+        IDs.clear();
+
+        Connection miConexion = (Connection) meConecto.ConectarMysql();
+
+        try (Statement st = miConexion.createStatement()) {
+
+            //Nuestra sentencia SQL
+            String sentencia = "SELECT * FROM `linea_pedido_cli` ORDER BY `Id_pedido` ASC";
+            Statement s = miConexion.createStatement();
+
+            //Almacenamos en un ResultSet
+            ResultSet rs = s.executeQuery(sentencia);
+
+            //Creando las filas para el JTable
+            while (rs.next()) {
+
+                if (!IDs.contains(rs.getInt("Id_pedido"))) {
+                    IDs.add(rs.getInt("Id_pedido"));
+                }
+
+            }
+            rs.close();
+            miConexion.close();
+        } catch (SQLException e) {
+            System.err.println("Se ha producido un Error! ");
+            System.err.println(e.getMessage());
+        }
+
+        for (Integer id : IDs) {
+            cmb_IDPedido.addItem(id);
+        }
+    }
+
+    public void Busqueda() {
+
+        int bucarA = (int) cmb_IDPedido.getSelectedItem();
+        int suma = 0;
+
+        Connection miConexion = (Connection) meConecto.ConectarMysql();
+
+        try (Statement st = miConexion.createStatement()) {
+
+            //Nuestra sentencia SQL
+            String sentencia = "SELECT * FROM `linea_pedido_cli` WHERE `Id_pedido` ='" + bucarA + "'";
+            Statement s = miConexion.createStatement();
+
+            //Almacenamos en un ResultSet
+            ResultSet rs = s.executeQuery(sentencia);
+
+            //Creando las filas para el JTable
+            while (rs.next()) {
+
+                precioNoIVA = rs.getInt("Total");
+                suma = suma + precioNoIVA;
+            }
+            rs.close();
+            miConexion.close();
+        } catch (SQLException e) {
+            System.err.println("Se ha producido un Error!: " + e);
+            System.err.println(e.getMessage());
+        }
+        precioNoIVA = suma;
+        txt_TotalnoIVA.setText(Float.toString(precioNoIVA));
     }
 }
